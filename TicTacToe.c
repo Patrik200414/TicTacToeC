d78a5becde459eb2be7a9b168890aca671ede86e
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-18 22:19:44 +0200

    Implement empty field checking

--- TicTacToe.c
+++ TicTacToe.c
@@ -23,26 +23,33 @@ int parseCoordinates(char coordinates[2]) {
     return (coordinates[0] - 'a') * 3 + coordinates[1] - '1';
 }
 
-int validateCoordinates(char coordinates[2]) {
+int validateCoordinates(char coordinates[2], char board[3][3]) {
     if (!isCharBetween(coordinates[0], 'a', 'c') || !isCharBetween(coordinates[1], '1', '3')) {
         return -1;
+    } 
+    
+    int arrayIndex = parseCoordinates(coordinates);
+
+    if(board[arrayIndex / 3][arrayIndex % 3] != ' '){
+        return -2;
     }
     // TODO: check if square is free
-    return parseCoordinates(coordinates);
+    return arrayIndex;
 }
 
-int readUserInput() {
+int readUserInput(char board[3][3]) {
     char coordinates[2];
     int result;
     while (1) {
         printf("Please provide the coordinates of your next move! ");
         // space at the beginning is necessary, otherwise it will be fed by the line feed
         scanf(" %c%c", coordinates, coordinates + 1);
-        result = validateCoordinates(coordinates);
+        result = validateCoordinates(coordinates, board);
         if (result == -1) {
             printf("Invalid coordinates! Valid coordinates range from a1 to c3\n");
         } else if (result == -2) {
             // TODO: use -2 for not empty square
+            printf("Invalid coordinates! An element already exists in this square!\n");
         } else {
             return result;
         }
@@ -57,11 +64,11 @@ int main(){
     };
     writeOutBoard(board);
     // some code for testing the new functionality
-    int input = readUserInput();
+    int input = readUserInput(board);
     board[input / 3][input % 3] = 'x';
     writeOutBoard(board);
     char* c = board;
-    input = readUserInput();
+    input = readUserInput(board);
     *(c + input) = 'x';
     writeOutBoard(board);
     return 0;
